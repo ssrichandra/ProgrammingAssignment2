d4c85480fa12bac350ad9698225ca3cbce59f4af
Sukanya Srichandra <ssrichan8@gmail.com>
2015-03-21 15:16:39 -0400

    2nd function completed with comments

--- cachematrix.R
+++ cachematrix.R
@@ -34,10 +34,38 @@ makeCacheMatrix <- function(matrix_data = matrix()) {
 }
 
 ## This function will compute the inverse of the matrix specified by user.
-        ##It will first check to see if an inverse is available in cache
-        ##If cached version exists, it will pull the cache, else it will calc
-        ##the inverse
+##It will first check to see if an inverse is available in cache
+##If cached version exists, it will pull the cache, else it will calc
+##the inverse
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(matrix_data, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        ##get stored inverse
+        stored_inverse<-matrix_data$getinv()
+        
+        ##check that the stored inverse has a value, i.e. was previously computed
+        if(!is.null(stored_inverse)) {
+                
+                #if previously computed, show the stored inverse
+                message("getting cached data...")
+                return(stored_inverse)
+                
+                
+        }
+        #if inverse wasn't previously computed, then
+        else {
+                
+                #add the new data to the environment
+                local_matrix<-matrix_data$get()
+                
+                #compute the inverse of the matrix data and put it in the stored inverse var.
+                stored_inverse<-solve(local_matrix)
+                
+                #put the stored inverse into the cache
+                matrix_data$setinv(stored_inverse)
+                return(stored_inverse)
+        }
+        
+        
 }
